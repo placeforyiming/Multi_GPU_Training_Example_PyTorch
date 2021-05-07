dcd04cb8bf98d038f9212363fa50b45f3718f68a
Yiming <emailfromyimingzhao@gmail.com>
2021-05-07 19:17:04 -0400

    add syncBN

--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 This is a simple example of pytorch DDP multi-gpu training on one machine.
 
-pull the docekr image : docker pull pytorch/pytorch:1.7.1-cuda11.0-cudnn8-runtime
+Pull the docekr image : docker pull pytorch/pytorch:1.7.1-cuda11.0-cudnn8-runtime
 
 docker run -it --name yiming_1 --gpus=all --shm-size=20G --mount type=bind,source=/mnt/VOL3/yiming,target=/workspace pytorch/pytorch:1.7.1-cuda11.0-cudnn8-runtime
+
+The train.py is the training script of single gpu, and the train_multi.py is the multi-gpu counterpart. All extra code has been commented with ##### multi_gpu need:.
+
+
+
--- train_multi.py
+++ train_multi.py
@@ -6,7 +6,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 import torch.optim as optim
-
+from net import *
 
 use_cuda = torch.cuda.is_available()
 ## load mnist dataset
@@ -71,46 +71,9 @@ test_loader = torch.utils.data.DataLoader(
 print ('==>>> total trainning batch number: {}'.format(len(train_loader)))
 print ('==>>> total testing batch number: {}'.format(len(test_loader)))
 
-## network
-class MLPNet(nn.Module):
-    def __init__(self):
-        super(MLPNet, self).__init__()
-        self.fc1 = nn.Linear(28*28, 500)
-        self.fc2 = nn.Linear(500, 256)
-        self.fc3 = nn.Linear(256, 10)
-    def forward(self, x):
-        x = x.view(-1, 28*28)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-    
-    def name(self):
-        return "MLP"
-
-class LeNet(nn.Module):
-    def __init__(self):
-        super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-    
-    def name(self):
-        return "LeNet"
 
 ## training
-model = LeNet()
+model = LeNet_BN()
 if use_cuda:
     model = model.cuda()
 
@@ -118,6 +81,7 @@ if use_cuda:
 
 ##### multi_gpu_need:   add model with DDP
 device=torch.device("cuda",arg_param.local_rank)
+model=torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 model=DDP(model.to(device),device_ids=[arg_param.local_rank],output_device=arg_param.local_rank)
 
 
